a9e1e9cec89fc9e0989a67462290dfbfe409e4c5
Gaurav Singh <gksingh947@gmail.com>
2024-11-13 00:03:06 +0530

    Fix Create User

--- src/lib/controllers/user.controller.ts
+++ src/lib/controllers/user.controller.ts
@@ -1,36 +1,43 @@
 import User from "../models/user.model";
-import {connectDB} from '../mongoDB/db';
+import { connectDB } from '../mongoDB/db';
 
 interface UserProps {
     id: string;
     first_name: string;
     last_name: string;
-
+    image_url: string;
+    email_addresses: { email_address: string }[];
+    username: string;
 }
+
 export const createOrUpdateUser = async (
-    id,
-    first_name,
-    last_name,
-    image_url,
-    email_addresses,
-    username
-    
-    
-) => {
+    id: string,
+    first_name: string,
+    last_name: string,
+    image_url: string,
+    email_addresses: { email_address: string }[],
+    username: string
+): Promise<UserProps | null> => {
     try {
         await connectDB();
-        const user = await User.findOneAndUpdate({clerkId: id},{
-            $set: {
-                firstName: first_name,
-                lastName: last_name,
-                avatar: image_url,
-                email: email_addresses[0].email_address,
-                username
-            }
-        }, {upsert: true, new: true});
 
-    
+        const user = await User.findOneAndUpdate(
+            { clerkId: id },
+            {
+                $set: {
+                    firstName: first_name,
+                    lastName: last_name,
+                    avatar: image_url,
+                    email: email_addresses[0]?.email_address,   
+                    username
+                }
+            },
+            { upsert: true, new: true }
+        );
+
+        return user;
     } catch (error) {
-        console.error(error);
+        console.error("Error creating or updating user:", error);
+        return null; 
     }
-}
+};
